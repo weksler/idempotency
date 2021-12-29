7e9beb8a87531174064179da38f772f7f4e448d2
Michel Weksler <mweksler@gmail.com>
2021-12-29 03:32:40 +0000

    google style

--- src/test/java/com/bablooka/idempotency/core/IdempotencyTest.java
+++ src/test/java/com/bablooka/idempotency/core/IdempotencyTest.java
@@ -7,15 +7,15 @@ import org.junit.Test;
 
 public class IdempotencyTest {
 
-    Idempotency idempotency;
+  Idempotency idempotency;
 
-    @Before
-    public void setUp() throws Exception {
-        idempotency = new Idempotency();
-    }
+  @Before
+  public void setUp() throws Exception {
+    idempotency = new Idempotency();
+  }
 
-    @Test
-    public void testGetName() {
-        assertEquals("Idempotency!", idempotency.getName());
-    }
+  @Test
+  public void testGetName() {
+    assertEquals("Idempotency!", idempotency.getName());
+  }
 }
