29aa83296e9d62e551dbadc65832970071fd470a
Michel Weksler <mweksler@gmail.com>
2022-01-28 16:05:08 -0800

    lowercase

--- resources/db/sql/V0001__create_transactions_table.sql
+++ resources/db/sql/V0001__create_transactions_table.sql
@@ -1,6 +1,6 @@
-create table TRANSACTIONS (
+create table transactions (
     id int not null,
     amount int not null,
-    status vatchar(100) not null,
+    status varchar(100) not null,
     description varchar(100)
 );
